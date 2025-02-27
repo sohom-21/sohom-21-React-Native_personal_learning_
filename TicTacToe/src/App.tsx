dd987e623a451174965847dc74d3f0ff6d184d54
Sohom Ghosh <sohom.official21@gmail.com>
2025-02-27 18:23:32 +0530

    Update changes

--- TicTacToe/src/App.tsx
+++ TicTacToe/src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import type {PropsWithChildren} from 'react';
 import {
+  Pressable,
   SafeAreaView,
   StyleSheet,
   Text,
@@ -9,18 +10,28 @@ import {
 
 
 
-type SectionProps = PropsWithChildren<{
+type TicTac = PropsWithChildren<{
   title: string;
 }>;
 
 
 
 function App(): React.JSX.Element {
-
+  const [playerTurn, setPlayerTurn] = useState(0);
   return (
     <SafeAreaView>
-      <View>
-        <Text>App</Text>
+      <View style={styles.mainElement}>
+      <View style={[styles.sectionContainer]}>
+        <View style={styles.sectionDescription}>
+          <Text style={styles.sectionTitle}>Player {playerTurn}'s Turn</Text>
+        </View>
+        <View>
+          <Text style={styles.sectionTitle}>GRid</Text>
+        </View>
+        <View>
+          <Pressable></Pressable>
+        </View>
+      </View>
       </View>
     </SafeAreaView>
   );
@@ -31,14 +42,20 @@ const styles = StyleSheet.create({
     marginTop: 32,
     paddingHorizontal: 24,
   },
+  mainElement:{
+    backgroundColor:'#0F0F0F',
+  },
   sectionTitle: {
     fontSize: 24,
     fontWeight: '600',
+    color:'#ffffff',
   },
   sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+    backgroundColor:'#d3c506',
+    padding:16,
+    borderRadius:4,
+    justifyContent:'center',
+    alignItems:'center',
   },
   highlight: {
     fontWeight: '700',
